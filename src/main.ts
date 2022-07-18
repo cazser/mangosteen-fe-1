1333510a3a426de68910e5dbe6b4ba6d8c0eb831
cazser <2264712072@qq.com>
2022-07-18 19:07:13 +0800

    router重构

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,7 @@
 import { createApp } from 'vue'
 import {App} from './App'
-import { Bar } from './views/Bar'
-import { Foo } from './views/Foo'
-import {createRouter, createWebHashHistory} from 'vue-router'
-const routes=[
-    {path:'/', component: Foo},
-    {path:'/bar', component: Bar}
-]
+import { router } from './route/router';
 
-const router =createRouter({
-    history: createWebHashHistory(),
-    routes
-})
 const app = createApp(App);
 app.use(router);
 app.mount('#app')
