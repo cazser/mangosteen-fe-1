8748e0f1cd73598b85e4410c8d3c3453f8334b48
cazser <2264712072@qq.com>
2022-07-22 16:23:09 +0800

    创建FloatButton

--- src/shared/FloatButton.tsx
+++ src/shared/FloatButton.tsx
@@ -1,14 +1,15 @@
 import { defineComponent, ref } from "vue";
 import add from '../assets/icons/add.svg'
 import { Icon } from "./Icon";
+import s from './FloatButton.module.scss'
 export const FloatButton = defineComponent(
     {
     setup(){
         
         return ()=>
-        <>
+        <button class={s.button}>
             <Icon src={add}/>
-         </>
+         </button>
         }
     }
 )
