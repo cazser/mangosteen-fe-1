b42e90efe23a1382cc27af6f235de0642dc32def
cazser <2264712072@qq.com>
2022-07-29 09:42:56 +0800

    计划使用grid布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -8,29 +8,33 @@ import 'vant/lib/index.css';
 export const InputPad = defineComponent(
   {
   setup(props, context){
-    const buttons=[
-      {text:'1', onclick:()=>{}},
-      {text:'2', onclick:()=>{}},
-      {text:'3', onclick:()=>{}},
-      {text:"清空", onclick:()=>{}},
-      {text:'4', onclick:()=>{}},
-      {text:'5', onclick:()=>{}},
-      {text:'6', onclick:()=>{}},
-      {text:"+", onclick:()=>{}},
-      {text:'7', onclick:()=>{}},
-      {text:'8', onclick:()=>{}},
-      {text:'9', onclick:()=>{}},
-      {text:"-", onclick:()=>{}},
-      {text:'.', onclick:()=>{}},
-      {text:'0', onclick:()=>{}},
-      {text:'删', onclick:()=>{}},
-      {text:'提交', onclick:()=>{}},
-    ];
-    const now = new Date()
+   const now = new Date()
     const refDate = ref<Date>(now);
       const refShowPop = ref(false);
       const hiddPopUp=()=>{refShowPop.value = false};
       const setDate =(date:Date)=>{refDate.value = date; hiddPopUp();}
+      const refAmount = ref("");
+      const appendText =(n: number | string)=>{refAmount.value += n.toString()}
+    
+  const buttons=[
+      {text:'1', onclick:()=>{appendText(1)}},
+      {text:'2', onclick:()=>{appendText(2)}},
+      {text:'3', onclick:()=>{appendText(3)}},
+      {text:"清空", onclick:()=>{refAmount.value=""}},
+      {text:'4', onclick:()=>{appendText(4)}},
+      {text:'5', onclick:()=>{appendText(5)}},
+      {text:'6', onclick:()=>{appendText(6)}},
+      {text:"+", onclick:()=>{}},
+      {text:'7', onclick:()=>{appendText(7)}},
+      {text:'8', onclick:()=>{appendText(8)}},
+      {text:'9', onclick:()=>{appendText(9)}},
+      {text:"-", onclick:()=>{}},
+      {text:'.', onclick:()=>{}},
+      {text:'0', onclick:()=>{appendText(0)}},
+      {text:'删', onclick:()=>{}},
+      {text:'提交', onclick:()=>{}},
+    ];
+   
       return ()=>
         <>
         <div class={[s.wrapper, s.dateAndAmount] }>
@@ -45,7 +49,7 @@ export const InputPad = defineComponent(
             </Popup>
           </span>
           </span>
-          <span class={s.amount}>199</span>
+          <span class={s.amount}>{refAmount.value}</span>
         </div>
         <div class={s.buttons}>
           {buttons.map((button)=><button onClick={button.onclick}>{button.text}</button>)}
