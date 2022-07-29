720d59b613d205930f881272ba6812bc8b48fa93
cazser <2264712072@qq.com>
2022-07-29 14:41:06 +0800

    +1

--- src/components/TagCreate.tsx
+++ src/components/TagCreate.tsx
@@ -16,9 +16,34 @@ export const TagCreate = defineComponent(
                return(
                 <div>
                   <form>
+                    <div>
                     <label>
+                      <span>标签名</span>
                       <input></input>
                     </label>
+                    </div>
+                    <div>
+                    <label>
+                      <span>符号</span>
+                      <div>
+                      
+                        <nav>
+                          <span>表情</span>
+                          <span>职业</span>
+                        </nav>
+                        <ol>
+                          <li >1</li >
+                          <li >2</li >
+                        </ol>
+                      </div>
+                    </label>
+                    </div>
+                    <div>
+                    <p>记账时长按标签即可进行编辑</p>
+                    </div>
+                    <div>
+                    <button>确定</button>
+                    </div>
                   </form>
                 </div>
                ) 
