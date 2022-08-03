aedd4cbb6b2d3199cc1cbcb2e93248a6c7a97a10
cazser <2264712072@qq.com>
2022-08-03 16:58:25 +0800

    +2

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -1,14 +1,27 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref, resolveComponent } from "vue";
 import { MainLayout } from "../layout/MainLayout";
 import { Icon } from "../shared/Icon";
 import left from '../assets/icons/left.svg'
 import s from './SignInPage.module.scss'
 import { Form, FormItem } from "../shared/Form";
 import { Button } from "../shared/Button";
+import { validate } from "../shared/validate";
 export const SignInPage = defineComponent(
   {
   setup(){
-      return ()=>
+    const formData = reactive({email:'', code:''});
+    const errors = reactive({email:[], code:[]})
+    const onSubmit=(e: Event)=>{
+      e.preventDefault();
+      Object.assign(errors, {email:[], code:[]})
+     Object.assign(errors, validate(formData, 
+        [
+        {key:"email", type:'required', message:"必填"},
+        {key:"code", type:'required', message:"必填"},
+        {key:"email", type:'pattern', regex:/.*@.*/, message:"必须是邮箱"}
+        ]));
+    }
+    return ()=>
         <>
         <MainLayout>
           {{
@@ -17,9 +30,11 @@ export const SignInPage = defineComponent(
             default:
               ()=><>
               <div class={s.wrapper}>
-                <Form>
-                  <FormItem label="邮箱地址" type="text"/>
-                  <FormItem label="验证码" type='validationCode'/>
+                <Form onSubmit={onSubmit}>
+                  <FormItem label="邮箱地址" type="text" 
+                  v-model={formData.email} error={errors.email?.[0] ?? ' '}/>
+                  <FormItem label="验证码" type='validationCode'
+                  v-model={formData.code} error={errors.code?.[0] ?? ' '}/>
                   <FormItem>
                     <Button>登录</Button>
                   </FormItem>
