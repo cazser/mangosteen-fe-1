db71b3448d73b5198dc78e2b3bc872b9ab113bda
cazser <2264712072@qq.com>
2022-08-04 13:18:28 +0800

    +1

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,4 +1,5 @@
 
+
 import { defineComponent, onMounted, PropType, ref } from "vue";
 import { FormItem } from "../../shared/Form";
 import s from './Charts.module.scss'
@@ -24,25 +25,23 @@ export const Charts = defineComponent(
 
       // 指定图表的配置项和数据
       var option = {
-        title: {
-          text: 'ECharts 入门示例'
-        },
-        tooltip: {},
-        legend: {
-          data: ['销量']
-        },
-        xAxis: {
-          data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-        },
-        yAxis: {},
-        series: [
-          {
-            name: '销量',
-            type: 'bar',
-            data: [5, 20, 36, 10, 10, 20]
-          }
-        ]
-      };
+          xAxis: {
+            type: 'category',
+            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+          },
+          grid:{
+            right: 0, left:0, top:0
+          },
+          yAxis: {
+            type: 'value'
+          },
+          series: [
+            {
+              data: [150, 230, 224, 218, 135, 147, 260],
+              type: 'line'
+            }
+          ]
+        };
 
       // 使用刚指定的配置项和数据显示图表。
       myChart.setOption(option);
