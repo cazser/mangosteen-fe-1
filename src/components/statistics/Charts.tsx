1da0ca47fb4ade1abbf729ec6031e644906dd57a
cazser <2264712072@qq.com>
2022-08-04 08:20:34 +0800

    +2

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -1,7 +1,17 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, PropType, ref } from "vue";
 import s from './Charts.module.scss'
 export const Charts = defineComponent(
   {
+ props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: true
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
   setup(){
       return ()=>
         <>
--- src/views/StatisticsPage.tsx
+++ src/views/StatisticsPage.tsx
@@ -1,10 +1,11 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent, reactive, ref } from "vue";
 import { MainLayout } from "../layout/MainLayout";
 import { Icon } from "../shared/Icon";
 import left from '../assets/icons/left.svg'
 import { RouterLink } from "vue-router";
 import { Tab, Tabs } from "../shared/Tabs";
 import { Charts } from "../components/statistics/Charts";
+import { Time } from "../shared/time";
 
 export const StatisticsPage = defineComponent(
   {
@@ -13,7 +14,15 @@ export const StatisticsPage = defineComponent(
       const onUpdateSelected=(name: string)=>{
             refKind.value = name;
         }
-      
+      const time = new Time();
+      const customTime= reactive({ 
+        start: new Time().format(),
+      end: new Time().format()});
+      const timeList =[
+        {start: time.firstDayOfMonth(), end: time.lastDayOfMonth()},
+        {start: time.add(-1, 'month').firstDayOfMonth(), end: time.add(-1, 'month').lastDayOfMonth()},
+        {start: time.firstDayOfYear(), end:time.lastDayOfYear()}
+      ]
       return ()=>
         <MainLayout>
           {
@@ -32,16 +41,16 @@ export const StatisticsPage = defineComponent(
                             onUpdateSelected={onUpdateSelected}
                             >
                             <Tab name="本月" >
-                              <Charts/>
+                              <Charts startDate={timeList[0].start.format()} endDate={timeList[0].end.format()}/>
                             </Tab>
                             <Tab name="上月" >
-                              <Charts />
+                              <Charts  startDate={timeList[1].start.format()} endDate={timeList[1].end.format()}/>
                             </Tab>
                              <Tab name="今年" >
-                              <Charts />
+                              <Charts startDate={timeList[2].start.format()} endDate={timeList[2].end.format()}/>
                             </Tab>
                             <Tab name="自定义时间" >
-                              <Charts />
+                              <Charts  startDate={customTime.start}  endDate={customTime.end}/>
                             </Tab>
                             </Tabs>
                   )
